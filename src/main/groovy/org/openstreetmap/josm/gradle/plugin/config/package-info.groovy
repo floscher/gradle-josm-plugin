f1a0586394b8987ab2a75bc4cde5cb3d41cecbc5
Florian Schäfer <florian@schaeferban.de>
2017-12-10 19:57:31 +0100

    Add package-info.groovy to the config package

---
+++ src/main/groovy/org/openstreetmap/josm/gradle/plugin/config/package-info.groovy
@@ -0,0 +1,7 @@
+/**
+ * The configuration options, that are available for use in the {@code build.gradle}.
+ *
+ * {@link JosmPluginExtension} contains the top-level options of the {@code josm{}} block.
+ * {@link I18nConfig} and {@link JosmManifest} deal with the nested blocks {@code i18n{}} and {@code manifest{}}
+ */
+package org.openstreetmap.josm.gradle.plugin.config
--- src/main/groovy/org/openstreetmap/josm/gradle/plugin/package-info.groovy
+++ src/main/groovy/org/openstreetmap/josm/gradle/plugin/package-info.groovy
@@ -4,3 +4,4 @@
  * It sets up commonly used repositories containing JOSM related dependencies.
  * Also some useful tasks are defined, e.g. for starting JOSM with the developed plugin and all required plugins loaded.
  */
+package org.openstreetmap.josm.gradle.plugin
