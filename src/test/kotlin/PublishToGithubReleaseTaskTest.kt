7cb5e8ef3eb8a0ba21ee3227c5da8e3160fbd5ab
Gubaer <karl.guggisberg@guggis.ch>
2018-06-03 17:17:23 +0200

    add tests for standard task 'publishToGithubRelease'

--- src/test/kotlin/PublishToGithubReleaseTaskTest.kt
+++ src/test/kotlin/PublishToGithubReleaseTaskTest.kt
@@ -33,25 +33,24 @@ class PublishToGithubReleaseTaskTest() : BaseGithubReleaseTaskTest() {
         sourceDir.mkdirs()
 
         val sourceFile = File(sourceDir, "TestPlugin.java")
-        sourceFile?.printWriter()?.use { out ->
+        sourceFile.printWriter().use { out ->
             out.println(testPluginContent)
         }
     }
 
     private fun prepareBuildFile(content: String) {
-        buildFile?.printWriter()?.use { out ->
-            out.println(content)
+        buildFile?.printWriter()?.use {
+            it.println(content)
         }
     }
 
     private fun prepareReleasesSpecs(content: String,
                                      releasesFile: File? = null) {
-        val releasesFile = releasesFile ?: File(buildDir, "releases.yml")
-        releasesFile?.printWriter()?.use { it.println(content) }
+        (releasesFile ?: File(buildDir, "releases.yml"))
+            .printWriter().use { it.println(content) }
     }
 
     @Test
-    @Disabled
     @ExtendWith(WiremockResolver::class, WiremockUriResolver::class)
     fun `with minimal configuration values - supplied on the command line`(
         @WiremockResolver.Wiremock server: WireMockServer,
@@ -145,7 +144,6 @@ class PublishToGithubReleaseTaskTest() : BaseGithubReleaseTaskTest() {
     }
 
     @Test
-    @Disabled
     @ExtendWith(WiremockResolver::class, WiremockUriResolver::class)
     fun `configured with task parameters, with custom remote jar name`(
         @WiremockResolver.Wiremock server: WireMockServer,
@@ -160,7 +158,6 @@ class PublishToGithubReleaseTaskTest() : BaseGithubReleaseTaskTest() {
 
         val localJarName = "test-$pluginVersion.jar"
         val remoteJarName = "test.jar"
-        val localJarPath = "${buildDir?.absolutePath}/build/dist/$localJarName"
 
         val buildFileContent = """
             plugins {
@@ -354,6 +351,215 @@ class PublishToGithubReleaseTaskTest() : BaseGithubReleaseTaskTest() {
         Assertions.assertEquals(
             TaskOutcome.SUCCESS,
             result.task(":myPublishToGithubRelease")?.outcome
+        )
+        println(result.output)
+    }
+
+    @Test
+    @ExtendWith(WiremockResolver::class, WiremockUriResolver::class)
+    fun `minimal config, on command line, use standard task`(
+        @WiremockResolver.Wiremock server: WireMockServer,
+        @WiremockUriResolver.WiremockUri uri: String) {
+
+        val minJosmVersion = 1111
+        val pluginVersion = 2222
+        val releaseId = 12345678
+        val releaseLabel = "v0.0.1"
+        val githubRepo = "repo_xy"
+
+        // the standard pattern for the jar name the gradle-josm-plugin sets
+        // for the plugin jar
+        val localJarName = "${buildDir?.name}-$pluginVersion.jar"
+
+        val buildFileContent = """
+            plugins {
+                id 'org.openstreetmap.josm' version '${pluginUnderTestVersion()}'
+                id 'java'
+            }
+            version = $pluginVersion
+            josm {
+              josmCompileVersion = "latest"
+              manifest {
+                  description = 'test'
+                  minJosmVersion = $minJosmVersion
+                  mainClass = 'test_plugin.TestPlugin'
+              }
+            }
+            // will invoke the standard task 'publishToGithubRelease'
+            """
+
+        val releasesContent = """
+              releases:
+                - label: $releaseLabel
+                  numeric_plugin_version: $pluginVersion
+                  numeric_josm_version: $minJosmVersion
+            """.trimIndent()
+
+
+
+        fun prepareAPIStub() {
+            val githubUser = System.getenv(ENV_VAR_GITHUB_USER)
+                ?: throw Exception(
+                    "env variable $ENV_VAR_GITHUB_USER not set"
+                )
+            // stub for "get releases"
+            val path1 = "/repos/$githubUser/$githubRepo/releases"
+            server.stubFor(get(WireMock.urlPathMatching("$path1.*"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withBody(
+                        """[{"id": $releaseId, "tag_name": "$releaseLabel"}]""")
+                )
+            )
+
+            // stub for "upload release asset"
+            val path2 = "/repos/$githubUser/$githubRepo/releases/$releaseId/assets"
+            server.stubFor(post(WireMock.urlPathMatching("$path2.*"))
+                .withHeader("Content-Type", equalTo(MEDIA_TYPE_JAR))
+                .withQueryParam("name", equalTo(localJarName))
+                //.withQueryParam("label", equalTo("test_plugin.jar"))
+                .willReturn(aResponse()
+                    .withStatus(201)
+                    .withBody("""{"id": 1}""")
+                )
+            )
+        }
+
+        prepareTestPluginSource()
+        prepareBuildFile(buildFileContent)
+        prepareReleasesSpecs(releasesContent)
+        prepareAPIStub()
+
+        val result = GradleRunner.create()
+            .withProjectDir(buildDir)
+            .withArguments("build", "publishToGithubRelease",
+                "--release-label", releaseLabel,
+                "--github-api-url", uri,
+                "--github-repository", githubRepo
+            )
+            .build()
+        Assertions.assertEquals(
+            TaskOutcome.SUCCESS,
+            result.task(":publishToGithubRelease")?.outcome
+        )
+    }
+
+
+    @Test
+    @ExtendWith(WiremockResolver::class, WiremockUriResolver::class)
+    fun `use standard task, full config in the task`(
+        @WiremockResolver.Wiremock server: WireMockServer,
+        @WiremockUriResolver.WiremockUri uri: String
+    ) {
+
+        val minJosmVersion = 1111
+        val pluginVersion = 2222
+        val releaseId = 12345678
+        val releaseLabel = "v0.0.1"
+        val githubRepo = "repo_xy"
+
+        val latestReleaseLabel = "most_recent"
+        val latestReleaseId = "45681234"
+
+        val localJarName = "test-$pluginVersion.jar"
+        val remoteJarName = "test.jar"
+
+        val buildFileContent = """
+            plugins {
+                id 'org.openstreetmap.josm' version '${pluginUnderTestVersion()}'
+                id 'java'
+            }
+            version = $pluginVersion
+            jar.archiveName = "$localJarName"
+            josm {
+              josmCompileVersion = "latest"
+              manifest {
+                  description = 'test'
+                  minJosmVersion = $minJosmVersion
+                  mainClass = 'test_plugin.TestPlugin'
+              }
+            }
+
+            publishToGithubRelease {
+                // githubUser from env var GITHUB_USER
+                // githubAccessToken from env var GITHUB_ACCESS_TOKEN
+                githubApiUrl = "$uri"
+                githubRepository = "$githubRepo"
+                releaseLabel = "$releaseLabel"
+                remoteJarName = "$remoteJarName"
+                // upload to latest release to
+                updateLatest = true
+            }
+            """
+
+        val releasesContent = """
+              latest_release:
+                name: $latestReleaseLabel
+              releases:
+                - label: $releaseLabel
+                  numeric_plugin_version: $pluginVersion
+                  numeric_josm_version: $minJosmVersion
+            """.trimIndent()
+
+
+        fun prepareAPIStub() {
+            val githubUser = System.getenv(ENV_VAR_GITHUB_USER)
+                ?: throw Exception(
+                    "env variable $ENV_VAR_GITHUB_USER not set"
+                )
+            // stub for "get releases"
+            val path1 = "/repos/$githubUser/$githubRepo/releases"
+            server.stubFor(get(WireMock.urlPathMatching("$path1.*"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withBody("""[
+                    |   {"id": $releaseId, "tag_name": "$releaseLabel"},
+                    |   {"id": $latestReleaseId,
+                    |    "tag_name": "$latestReleaseLabel"
+                    |   }
+                    |]""".trimMargin())
+                )
+            )
+
+            // stub for "upload release asset"
+            val path2 = "/repos/$githubUser/$githubRepo/releases/" +
+                releaseId + "/assets"
+            server.stubFor(post(WireMock.urlPathMatching("$path2.*"))
+                .withHeader("Content-Type", equalTo(MEDIA_TYPE_JAR))
+                .withQueryParam("name", equalTo(remoteJarName))
+                //.withQueryParam("label", equalTo("test_plugin.jar"))
+                .willReturn(aResponse()
+                    .withStatus(201)
+                    .withBody("""{"id": 1}""")
+                )
+            )
+
+            // stub for "upload release asset" to the latest release
+            val path3 = "/repos/$githubUser/$githubRepo/releases/" +
+                latestReleaseId + "/assets"
+            server.stubFor(post(WireMock.urlPathMatching("$path3.*"))
+                .withHeader("Content-Type", equalTo(MEDIA_TYPE_JAR))
+                .withQueryParam("name", equalTo(remoteJarName))
+                //.withQueryParam("label", equalTo("test_plugin.jar"))
+                .willReturn(aResponse()
+                    .withStatus(201)
+                    .withBody("""{"id": 2}""")
+                )
+            )
+        }
+
+        prepareBuildFile(buildFileContent)
+        prepareTestPluginSource()
+        prepareReleasesSpecs(releasesContent)
+        prepareAPIStub()
+
+        val result = GradleRunner.create()
+            .withProjectDir(buildDir)
+            .withArguments("build", "publishToGithubRelease")
+            .build()
+        Assertions.assertEquals(
+            TaskOutcome.SUCCESS,
+            result.task(":publishToGithubRelease")?.outcome
         )
         println(result.output)
     }
