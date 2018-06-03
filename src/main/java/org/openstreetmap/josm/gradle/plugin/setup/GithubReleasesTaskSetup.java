6742fa0e004506c1dc8d7861c93edc50bd0717f8
Gubaer <karl.guggisberg@guggis.ch>
2018-06-03 17:07:08 +0200

    configure a task 'publishToGithubRelease' provided by the gradle plugin

--- src/main/java/org/openstreetmap/josm/gradle/plugin/setup/GithubReleasesTaskSetup.java
+++ src/main/java/org/openstreetmap/josm/gradle/plugin/setup/GithubReleasesTaskSetup.java
@@ -3,6 +3,7 @@ package org.openstreetmap.josm.gradle.plugin.setup;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.openstreetmap.josm.gradle.plugin.task.CreateGithubReleaseTask;
+import org.openstreetmap.josm.gradle.plugin.task.PublishToGithubReleaseTask;
 
 public class GithubReleasesTaskSetup {
   private final Project project;
@@ -18,6 +19,15 @@ public class GithubReleasesTaskSetup {
     );
     createGithubReleaseTask.setDescription(
       "Creates a new Github release using the Github API"
+    );
+
+    final Task publishToGithubReleaseTask = project.getTasks().create(
+      "publishToGithubRelease",
+      PublishToGithubReleaseTask.class
+    );
+    publishToGithubReleaseTask.setDescription(
+      "Publish a JOSM plugin jar as Github release asset to a " +
+      "Github release"
     );
   }
 }
