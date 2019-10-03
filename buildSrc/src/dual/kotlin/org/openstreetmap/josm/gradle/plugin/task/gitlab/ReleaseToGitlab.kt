2d612ae5345f47f25dfc0afe5dfd9b8477e33f81
Florian Schäfer <florian@schaeferban.de>
2019-10-04 01:05:54 +0200

    Increase entries per page for paginated API endpoints
    That way we hopefully don't need to worry about querying one endpoint multiple times if not all entries fit on one page.

--- buildSrc/src/dual/kotlin/org/openstreetmap/josm/gradle/plugin/task/gitlab/ReleaseToGitlab.kt
+++ buildSrc/src/dual/kotlin/org/openstreetmap/josm/gradle/plugin/task/gitlab/ReleaseToGitlab.kt
@@ -73,7 +73,7 @@ open class ReleaseToGitlab @Inject constructor(
       ?: try {
         logger.lifecycle("Getting the project path from the GitLab API at ${gitlabSettings.gitlabApiUrl} …")
         Json.nonstrict.parse(ProjectInfo.serializer(), URL("${gitlabSettings.gitlabApiUrl}/projects/${gitlabSettings.projectId}").readText())
-          .apply { logger.lifecycle("Retrieved project path for project ${gitlabSettings.projectId} from GitLab API: $this") }.projectPath
+          .apply { logger.lifecycle("Retrieved project path for project ${gitlabSettings.projectId} from GitLab API: ${this.projectPath}") }.projectPath
       } catch (e: FileNotFoundException) {
         throw TaskExecutionException(this, IllegalArgumentException("Project ${gitlabSettings.projectId} not found via API ${gitlabSettings.gitlabApiUrl}!"))
       }
@@ -96,10 +96,10 @@ open class ReleaseToGitlab @Inject constructor(
     logger.lifecycle("Found git tag ${revTag.tagName}: ${revTag.shortMessage}")
 
     // Find all matching packages available on GitLab
-    val assetLinks = Json.nonstrict.parse(Package.serializer().list, URL("${gitlabSettings.gitlabApiUrl}/projects/${gitlabSettings.projectId}/packages").readText())
+    val assetLinks = Json.nonstrict.parse(Package.serializer().list, URL("${gitlabSettings.gitlabApiUrl}/projects/${gitlabSettings.projectId}/packages?per_page=10000").readText())
       .filter { artifactVersion == it.version && names.contains(it.name) }
       .flatMap { packg ->
-        Json.nonstrict.parse(PackageFile.serializer().list, URL("${gitlabSettings.gitlabApiUrl}/projects/${gitlabSettings.projectId}/packages/${packg.id}/package_files").readText())
+        Json.nonstrict.parse(PackageFile.serializer().list, URL("${gitlabSettings.gitlabApiUrl}/projects/${gitlabSettings.projectId}/packages/${packg.id}/package_files?per_page=10000").readText())
           .filter { it.fileName.endsWith(".jar") }
           .map { ReleaseAssetLink(it.fileName, "${gitlabSettings.gitlabUrl}/$projectPath/-/package_files/${it.id}/download") }
       }
