81ade90995d8357745f628f7fe28477b8342641e
Taylor Smock <tsmock@fb.com>
2022-01-05 12:51:31 -0700

    Update Jackson from 2.13.0 to 2.13.1
    Signed-off-by: Taylor Smock <tsmock@fb.com>

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -8,7 +8,7 @@ public final class Versions {
   public static final String kotlinxHtml = "0.7.3";
   public static final String dokka = "1.6.0";
   public static final String kotlinSerialization = "1.3.1";
-  public static final String jackson = "2.13.0";
+  public static final String jackson = "2.13.1";
   public static final String jacoco = "0.8.7";
   public static final String jgit = "6.0.0.202111291000-r";
   public static final String junit = "5.8.2";
